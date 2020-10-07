bd48cb738ea9966ea211f56c2d30f2493aeafa70
Manas Kulkarni <manas.msk8067@gmail.com>
2020-10-07 18:33:08 -0400

    testing server files

--- job-application-tracker/src/routes/index.js
+++ job-application-tracker/src/routes/index.js
@@ -4,7 +4,7 @@ const constructorMethod = (app) => {
   app.use("/api/newjob", newJob);
 
   app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not found" });
+    res.status(404).json({ error: "Page Not found" });
   });
 };
 
