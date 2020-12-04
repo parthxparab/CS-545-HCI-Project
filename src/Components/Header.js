fe468eed0f39ff196de648dc6d51897ca620889e
parthxparab <parthpparab@gmail.com>
2020-12-04 02:28:54 -0500

    Major UI Changes
    Added About us and help data. Added Icon button and hyperlink

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,52 +5,63 @@ import HelpIcon from '@material-ui/icons/Help';
 import InfoIcon from '@material-ui/icons/Info';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 
+import logo from '../images/logo.png';
+
 const useStyles = makeStyles((theme) => ({
-	grow: {
-		flexGrow: 1,
-	},
-	title: {
-		flexGrow: 1,
-		color: '#2e3338',
-		backgroundColor: '#ede1d4',
-	},
-	bar: {
-		backgroundColor: '#ede1d4',
-	},
-	sectionDesktop: {
-		display: 'none',
-		[theme.breakpoints.up('md')]: {
-			display: 'flex',
-		},
-	},
-	sectionMobile: {
-		display: 'flex',
-		[theme.breakpoints.up('md')]: {
-			display: 'none',
-		},
-	},
-	button: {
-		color: '#2e3338',
-	},
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    color: '#2e3338',
+    backgroundColor: '#ede1d4',
+  },
+  bar: {
+    backgroundColor: '#ede1d4',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  button: {
+    color: '#2e3338',
+  },
 }));
 
 function Header(props) {
-	const { setOpen } = props;
-	const classes = useStyles();
+  const { setOpen } = props;
+  const classes = useStyles();
 
-	return (
-		<div>
-			<AppBar className={classes.bar}>
-				<Toolbar>
-					<Button
-						onClick={() => {
-							setOpen('Home');
-						}}
-						color='default'
-					>
-						<Box
+  return (
+    <div>
+      <AppBar className={classes.bar}>
+        <Toolbar>
+          <IconButton
+            onClick={() => {
+              setOpen('Home');
+            }}
+            color='default'
+          >
+            {' '}
+            <img
+              src={logo}
+              className='App-logo'
+              alt='logo'
+              height='40'
+              width='80'
+            />
+            {/* <Box
 							fontWeight='fontWeightBold'
 							fontSize='h5.fontSize'
 							textAlign='left'
@@ -58,35 +69,37 @@ function Header(props) {
 							m={1}
 						>
 							TRACE
-						</Box>
-					</Button>
-
-					<div className={classes.grow} />
-					<div className={classes.sectionDesktop}>
-						<Button
-							onClick={() => {
-								setOpen('Help');
-							}}
-							color='default'
-							startIcon={<HelpIcon />}
-						>
-							Help
-						</Button>
-						<Button
-							onClick={() => {
-								setOpen('AboutUs');
-							}}
-							color='default'
-							startIcon={<InfoIcon />}
-						>
-							About Us
-						</Button>
-					</div>
-					<div className={classes.sectionMobile}></div>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+						</Box> */}
+          </IconButton>
+          <Box fontFamily='Raleway' m={1} color='black'>
+            The Job Application Tracker
+          </Box>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button
+              onClick={() => {
+                setOpen('Help');
+              }}
+              color='default'
+              startIcon={<HelpIcon />}
+            >
+              Help
+            </Button>
+            <Button
+              onClick={() => {
+                setOpen('AboutUs');
+              }}
+              color='default'
+              startIcon={<InfoIcon />}
+            >
+              About Us
+            </Button>
+          </div>
+          <div className={classes.sectionMobile}></div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Header;
